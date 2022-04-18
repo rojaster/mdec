d57a30110901e6cd6948e18d08cc015f7b97b722
Rojaster(alekum) <rojaster@yandex.ru>
2022-04-18 06:42:47 +0400

    Add RetDec LLVM IR dump
    - [FIXME] fix issue when .c file should be specified to dump .ll
    
    - [TODO] add option to dump .dsm file or output from 'retdec' tool
    
    ----
    
    Also, Snowman requires somefixes around IR printing as it prints DOT output only mixing DOT format directives and IR instructions.

--- backend/retdec/mdec-retdec/mdecretdec/service.py
+++ backend/retdec/mdec-retdec/mdecretdec/service.py
@@ -8,13 +8,25 @@ class RetdecService(Service):
     """
     RetDec decompiler as a service
     """
+    def process(self, path: str, suffix: str) -> str:
+        """
+        Process given binary and dump .c, .ll files
+        Use '.c' as output, because it dumps '.ll' also
+        """
+        subprocess.run(['/opt/retdec/bin/retdec-decompiler', '-o', path + '.c', path])
+        return open(path + suffix).read()
+
+    def lifting(self, path: str) -> str:
+        """
+        Read .ll dumped IR after processing given file.
+        """
+        return self.process(path, '.ll')
 
     def decompile(self, path: str) -> str:
         """
         Decompile all the functions in the binary located at `path`.
         """
-        subprocess.check_output(['/opt/retdec/bin/retdec-decompiler', path]).decode('utf-8')
-        return open(path + '.c').read()
+        return self.process(path, '.c')
 
     def version(self) -> str:
         output = subprocess.check_output(['/opt/retdec/bin/retdec', '--version']).decode('utf-8')
